1433d9ce7bea632bdec01a3048d1c23126394943
Joe Whales <joewhales0@gmail.com>
2024-08-20 23:14:00 +0200

    Updated pipeline

--- pipeline.bat
+++ pipeline.bat
@@ -1,14 +1,6 @@
-@REM @echo off
-@REM python .\Preprocessing\chunker.py preprocess_config-1.yaml
-@REM python .\Preprocessing\calc_stats.py --separate chunks/ 1
-@REM python .\Preprocessing\train_test_split.py chunks/
-@REM python .\Preprocessing\scale_data_multi.py
-@REM python .\Preprocessing\generate_metadata.py
-@REM python .\UniAD\train_val.py --config train_config.yaml
-@REM pause
 @echo off
-python .\Preprocessing\chunker.py preprocess_config.yaml
+python .\Preprocessing\chunker.py preprocess_config-big.yaml
 python ./Preprocessing/process_chunks.py chunks chunks_scaled
 python .\Preprocessing\generate_metadata.py chunks_scaled -t
-@REM python .\UniAD\train_val.py --config train_config.yaml
+python .\UniAD\train_val.py --config train_config.yaml
 pause 
